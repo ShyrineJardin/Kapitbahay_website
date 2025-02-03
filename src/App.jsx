8e8453be29e5baa07c481a1796031468d50e6830
ShyrineJardin <jardinshyrine@gmail.com>
2025-02-03 15:17:09 +0800

    adding react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+  return (
+    <>
+    <header className='bg-red-500'>
+
+      <div className="container">
+      <h1 class="text-3xl font-bold underline text-start bg-red-100">Hello world!</h1>
+      </div>
+    </header>
+   
+      
+    </>
+  )
+}
+
+export default App
