fc7d346d3d2e675eb0e849afe53211b1fdc08905
ShyrineJardin <jardinshyrine@gmail.com>
2025-02-06 15:07:27 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
 import OverviewCounter from './components/overview-counter/OverviewCounter';
-import About from './components/about/about';
+import About from './components/about/About';
 import Services from './components/services/Services';
 import Install from './components/install/Install';
 import Blogs from './components/blogs/Blogs';
